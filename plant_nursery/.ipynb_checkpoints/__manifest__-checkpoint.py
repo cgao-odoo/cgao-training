53cc20eeeac1dec9aa0baf558f3583f0f0dd6293
cgao <cgao@odoo.com>
2021-10-08 00:47:41 +0000

    rebuild manifest

---
+++ plant_nursery/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Plant Nursery",
+    'version': "1.0",
+    'category: "Tools",
+    'summary': "Plant Management",
+    'depends': ['base'], 
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'data/data.xml',
+    ]
+}
---
+++ plant_nursery/models/.ipynb_checkpoints/nursery-checkpoint.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class Plants(models.Model):
+    _name = 'nursery.plant'
+    
+    name = fields.Char(string='Plant Name')
+    price = fields.Float()
